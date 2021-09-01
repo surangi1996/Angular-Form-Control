9d8b3c2321afef2e1fd40a8c28991e451717af3b
surangi1996 <55706550+surangi1996@users.noreply.github.com>
2021-09-01 23:14:07 +0530

    angular form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularFormControl'`, () => {
+  it(`should have as title 'angularMaterial'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularFormControl');
+    expect(app.title).toEqual('angularMaterial');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularFormControl app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularMaterial app is running!');
   });
 });
