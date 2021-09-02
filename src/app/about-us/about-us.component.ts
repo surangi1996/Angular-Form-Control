dc220d0f946b97ce10b2da081bb533907faad950
surangi1996 <55706550+surangi1996@users.noreply.github.com>
2021-09-02 11:59:51 +0530

    interface create and data one way data binding

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Employee {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent implements OnInit {
+  employees: Employee[] = [
+    {id: "1", email: "test@gmail.com", name: "surangi", password: "1234"},
+    {id: "2", email: "test1@gmail.com", name: "kanchana", password: "12345"},
+    {id: "3", email: "test2@gmail.com", name: "kamal", password: "123456"}
+  ]
 
   constructor() { }
 
   ngOnInit(){
   }
 
-  show(){
-    alert('Hello Surangi');
-  }
+
 
 }
